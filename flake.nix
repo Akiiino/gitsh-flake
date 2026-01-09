304b8cc1c4090b0285cf82d2b7992599fa20cf37
akiiino <git@akiiino.me>
2026-01-09 12:52:13 +0100

    chore: modernize
    Bring the flake into present with `nixos-25.11` and
    remove unnecessary flake-parts input

--- flake.nix
+++ flake.nix
@@ -2,39 +2,46 @@
   description = "A flake for thoughtbot/gitsh";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-    gitsh-release.url = "https://github.com/thoughtbot/gitsh/releases/download/v0.14/gitsh-0.14.tar.gz";
-    gitsh-release.flake = false;
+    gitsh-release = {
+      url = "https://github.com/thoughtbot/gitsh/releases/download/v0.14/gitsh-0.14.tar.gz";
+      flake = false;
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-parts,
-    gitsh-release,
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      gitsh-release,
+    }:
+    let
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
         "aarch64-linux"
         "aarch64-darwin"
       ];
-      perSystem = {
-        pkgs,
-        self',
-        ...
-      }: {
-        formatter = pkgs.alejandra;
-        packages.gitsh = pkgs.stdenv.mkDerivation rec {
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt);
+
+      packages = forAllSystems (pkgs: rec {
+        gitsh = pkgs.stdenv.mkDerivation rec {
           pname = "gitsh";
           version = "0.14";
 
           src = gitsh-release;
 
-          buildInputs = [pkgs.gcc pkgs.readline pkgs.ruby];
+          buildInputs = [
+            pkgs.gcc
+            pkgs.readline
+            pkgs.ruby
+          ];
+
+          env.NIX_CFLAGS_COMPILE = "-Wno-error=incompatible-pointer-types"; # "fix" for newer ruby
 
           preConfigure = ''
             export RUBY=${pkgs.ruby}/bin/ruby
@@ -42,14 +49,14 @@
             export LDFLAGS="-L${pkgs.readline}/lib"
           '';
 
-          makeFlags = ["PREFIX=$(out)"];
+          makeFlags = [ "PREFIX=$(out)" ];
 
           installPhase = ''
             mkdir -p $out/bin
             make install
           '';
         };
-        packages.default = self'.packages.gitsh;
-      };
+        default = gitsh;
+      });
     };
 }
